34ebb474d23e5a0aa97c036357f46e49fc31c82f
guystrelitz@gmail.com <guystrelitz@gmail.com>
2020-07-16 18:45:59 +0100

    added a shopping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :title, uniqueness: true
@@ -11,5 +14,15 @@ class Product < ApplicationRecord
 			within: 10..500, 
 			too_short: 'must be at least 10 characters long.', 
 			too_long: 'must be no more than 500 characters long.'
-		}
+	 	}
+
+	private
+
+	def ensure_not_referenced_by_any_line_item
+		unless line_items.empty?
+			errors.add :base, 'Line Items present'
+			throw :abort
+		end
+	end
+
 end
