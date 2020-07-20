e8fda64c74d705cfb124c8251609d1b3d2274f73
guystrelitz@gmail.com <guystrelitz@gmail.com>
2020-07-20 13:24:15 +0100

    established a socket to keep product prices updated in real time

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # enable channels from other machines in dev mode
+  config.action_cable.disable_request_forgery_protection = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
