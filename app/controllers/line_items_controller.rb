48364695cd55523d353bfb08c548856f35b1373e
guystrelitz@gmail.com <guystrelitz@gmail.com>
2020-07-20 10:48:02 +0100

    amalgamated app/views/line_items/create.js.erb and destroy.js.erb into line_items/cart_ajax.js.erb

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,8 +32,11 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
-        format.js   { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
+        format.js do
+          @current_item = @line_item
+          render template: 'line_items/cart_ajax.js.erb'
+        end
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
@@ -67,8 +70,11 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to store_index_url }
-      format.js   { @current_item = @line_item }
       format.json { head :no_content }
+      format.js do
+        @current_item = @line_item
+        render template: 'line_items/cart_ajax.js.erb'
+      end
     end
   end
 
