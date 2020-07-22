5de389eebd8c67ddc7b2702b0af207510dcf742d
guystrelitz@gmail.com <guystrelitz@gmail.com>
2020-07-22 16:22:32 +0100

    limited login/out functionality – there's no authorisation yet and no way yet to log out

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get login_url
+    assert_response :success
+  end
+
+  test 'should log in' do
+    susannah = users :one
+    post login_url, params: { name: susannah.name, password: 'secret' }
+    assert_redirected_to admin_url
+    asert_equal susannah.id, session[:user_id]
+  end
+
+  test 'should fail login' do
+    susannah = users :one
+    post login_url, params: { name: susannah.name, password: 'wrong' }
+    assert_redirected_to login_url
+    assert_nil session[:user_id]
+  end
+
+  test 'should log out' do
+    delete logout_url
+    assert_redirected_to store_index_url
+  end
+
+end
